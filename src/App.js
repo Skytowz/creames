2fc9ea9ab646a40560f8e3eb67d24b7cb18128fc
Skytowz <lucashottin359@gmail.com>
2022-08-09 15:18:40 +0200

    Ajout des routes de la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Accueil from './pages/Accueil/Accueil';
+import CreerJeu from './pages/CreerJeu/CreerJeu';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path='/' element={<Accueil/>}/>
+          <Route path='/jouer' element={<p>coucou</p>}/>
+          <Route path='/creer-jeu' element={<CreerJeu/>}/>
         </Routes>
       </div>
     </BrowserRouter>
