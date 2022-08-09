6f8113193c03e95a597bb1fb3831e54479079215
Skytowz <lucashottin359@gmail.com>
2022-08-09 15:35:53 +0200

    Création de la page de création de jeu

---
+++ src/pages/CreerJeu/CreerJeu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Template from '../../components/template/template';
+import './CreerJeu.css';
+
+const CreerJeu = (props) => {
+    return(
+        <Template>
+            <p>Coucou TOA</p>
+        </Template>
+    )
+}
+
+export default CreerJeu;
